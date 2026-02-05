1b2bef23b7037a7f633f36e771bd2c9335556305
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-04 21:31:02 -0600

    feat: add relic items

---
+++ src/WotGInfernumPatch/Content/Relics/InfernumRelics.cs
@@ -0,0 +1,37 @@
+﻿using InfernumMode.Content.Items.Relics;
+using InfernumMode.Content.Tiles.Relics;
+using Terraria.ModLoader;
+
+namespace WotGInfernumPatch.Content.Relics;
+
+public sealed class NamelessDeityRelicInfernumItem : BaseRelicItem
+{
+    public override string Texture => "WotGInfernumPatch/Content/Relics/NamelessDeityRelicInfernum_Item";
+
+    public override string DisplayNameToUse => "Infernal Nameless Deity Relic";
+
+    public override int TileID => ModContent.TileType<NamelessDeityRelicInfernumTile>();
+}
+
+public sealed class NamelessDeityRelicInfernumTile : BaseInfernumBossRelic
+{
+    public override string RelicTextureName => "WotGInfernumPatch/Content/Relics/NamelessDeityRelicInfernum_Tile";
+
+    public override int DropItemID => ModContent.ItemType<NamelessDeityRelicInfernumItem>();
+}
+
+public sealed class NoxusRelicInfernumItem : BaseRelicItem
+{
+    public override string Texture => "WotGInfernumPatch/Content/Relics/NoxusRelicInfernum_Item";
+
+    public override string DisplayNameToUse => "Infernal Avatar of Emptiness Relic";
+
+    public override int TileID => ModContent.TileType<NoxusRelicInfernumTile>();
+}
+
+public sealed class NoxusRelicInfernumTile : BaseInfernumBossRelic
+{
+    public override string RelicTextureName => "WotGInfernumPatch/Content/Relics/NoxusRelicInfernum_Tile";
+
+    public override int DropItemID => ModContent.ItemType<NoxusRelicInfernumItem>();
+}
