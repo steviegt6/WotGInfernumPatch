43e092e8ca2283862eebbce7236c5250f96b40e5
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-07 00:36:17 -0600

    feat: aggressive frost columns

--- src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/WhirlingIceStormChanges.cs
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/WhirlingIceStormChanges.cs
@@ -14,8 +14,10 @@ internal sealed class WhirlingIceStormChanges : ModSystem
 {
     public static float InitialArcticBlastMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 1.5f : 1f;
 
-    public static float BlastReleaseRateMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.33f : 1f;
-    
+    public static float BlastReleaseRateMultiplier => 1f; /*InfernumMode.InfernumMode.CanUseCustomAIs ? 0.33f : 1f;*/
+
+    public static float FrostColumnReleaseRateMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.75f : 1f;
+
     public override void Load()
     {
         base.Load();
@@ -25,7 +27,7 @@ internal sealed class WhirlingIceStormChanges : ModSystem
             typeof(Avatar).GetMethod(nameof(Avatar.DoBehavior_WhirlingIceStorm), BindingFlags.Public | BindingFlags.Instance)!,
             SpawnMoreInitialArcticBlasts
         );
-        
+
         // Let arctic blasts go a lot faster.
         MonoModHooks.Add(
             typeof(ArcticBlast).GetMethod(nameof(ArcticBlast.SpinAround), BindingFlags.Public | BindingFlags.Static)!,
@@ -35,6 +37,11 @@ internal sealed class WhirlingIceStormChanges : ModSystem
         MonoModHooks.Modify(
             typeof(Avatar).GetMethod(nameof(Avatar.DoBehavior_WhirlingIceStorm), BindingFlags.Public | BindingFlags.Instance)!,
             SpawnArcticBlastsMoreFrequently
+        );
+
+        MonoModHooks.Modify(
+            typeof(Avatar).GetMethod(nameof(Avatar.DoBehavior_WhirlingIceStorm), BindingFlags.Public | BindingFlags.Instance)!,
+            SpawnFrostColumnsMoreFrequently
         );
     }
 
@@ -57,7 +64,7 @@ internal sealed class WhirlingIceStormChanges : ModSystem
         var velocityBaseline = InfernumMode.InfernumMode.CanUseCustomAIs
             ? 32f
             : 20f;
-        
+
         var spinCompletion = Utilities.InverseLerp(0f, ArcticBlast.Lifetime - 60, time);
         var maxHorizontalSpeed = MathF.Cos(MathHelper.TwoPi * startingYPosition / 1040f) * velocityVariance + velocityBaseline;
         var spinArc = MathHelper.Pi * spinCompletion * 4f;
@@ -68,10 +75,22 @@ internal sealed class WhirlingIceStormChanges : ModSystem
     private static void SpawnArcticBlastsMoreFrequently(ILContext il)
     {
         var c = new ILCursor(il);
-        
+
         c.GotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.WhirlingIceStorm_BlastReleaseRate)}"));
         c.EmitDelegate(
             (int blastReleaseRate) => (int)(blastReleaseRate * BlastReleaseRateMultiplier)
         );
+    }
+
+    private static void SpawnFrostColumnsMoreFrequently(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        while (c.TryGotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.WhirlingIceStorm_FrostColumnReleaseRate)}")))
+        {
+            c.EmitDelegate(
+                (int frostColumnReleaseRate) => (int)(frostColumnReleaseRate * FrostColumnReleaseRateMultiplier)
+            );
+        }
     }
 }
