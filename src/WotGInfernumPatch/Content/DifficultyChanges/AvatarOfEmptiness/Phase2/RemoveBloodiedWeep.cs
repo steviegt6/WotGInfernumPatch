0cd2b895300355859145a534df107a6051c4bb22
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-06 23:49:35 -0600

    feat: remove bloodied weep in infernum

---
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase2/RemoveBloodiedWeep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Terraria.ModLoader;
+using Avatar = NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm.AvatarOfEmptiness;
+
+namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase2;
+
+internal sealed class RemoveBloodiedWeep : ModSystem
+{
+    public override void Load()
+    {
+        base.Load();
+
+        MonoModHooks.Add(
+            typeof(Avatar).GetMethod(nameof(Avatar.ChooseNewPattern), BindingFlags.Public | BindingFlags.Instance)!,
+            ChooseNewPattern_FilterOutBloodiedWeep
+        );
+    }
+
+    private static List<Avatar.AvatarAIType> ChooseNewPattern_FilterOutBloodiedWeep(Func<Avatar, List<Avatar.AvatarAIType>> orig, Avatar self)
+    {
+        var attacks = orig(self);
+
+        return !InfernumMode.InfernumMode.CanUseCustomAIs
+            ? attacks
+            : attacks.Where(x => x != Avatar.AvatarAIType.BloodiedWeep).ToList();
+    }
+}
