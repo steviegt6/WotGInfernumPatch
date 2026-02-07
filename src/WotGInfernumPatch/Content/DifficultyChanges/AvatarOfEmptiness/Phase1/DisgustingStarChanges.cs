bd6c2b193825d4632ab1a97ae8b76ca202913cff
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-06 21:51:55 -0600

    feat: spawn more disgusting stars

---
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase1/DisgustingStarChanges.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using MonoMod.Cil;
+using Terraria.ModLoader;
+using AvatarRift = NoxusBoss.Content.NPCs.Bosses.Avatar.FirstPhaseForm.AvatarRift;
+
+namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase1;
+
+internal sealed class DisgustingStarChanges : ModSystem
+{
+    public static int StarReleaseRate => InfernumMode.InfernumMode.CanUseCustomAIs ? 2 : 3;
+    
+    public override void Load()
+    {
+        base.Load();
+
+        MonoModHooks.Modify(
+            typeof(AvatarRift).GetMethod(nameof(AvatarRift.DoBehavior_RedirectingDisgustingStars), BindingFlags.Public | BindingFlags.Instance)!,
+            SpawnMoreDisgustingStars
+        );
+    }
+
+    private static void SpawnMoreDisgustingStars(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        // starReleaseRate = 3
+        c.GotoNext(MoveType.After, x => x.MatchLdcI4(3));
+        c.EmitPop();
+        c.EmitDelegate(() => StarReleaseRate);
+    }
+}
