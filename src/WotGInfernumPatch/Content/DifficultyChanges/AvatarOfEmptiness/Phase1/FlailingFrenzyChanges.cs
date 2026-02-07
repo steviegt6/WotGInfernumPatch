dd0232fc6d508cdc63daae4b267e2a26c0a7432e
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-02-06 22:40:19 -0600

    feat: make flailing frenzy portal hands more aggressive

---
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase1/FlailingFrenzyChanges.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using MonoMod.Cil;
+using Terraria.ModLoader;
+using AvatarRift = NoxusBoss.Content.NPCs.Bosses.Avatar.FirstPhaseForm.AvatarRift;
+
+namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase1;
+
+internal sealed class FlailingFrenzyChanges : ModSystem
+{
+    public static int PortalSummonDelay => InfernumMode.InfernumMode.CanUseCustomAIs ? 64 : 76;
+
+    public static int PortalSummonRate => InfernumMode.InfernumMode.CanUseCustomAIs ? 6 : 8;
+
+    public static int PortalLifetime => InfernumMode.InfernumMode.CanUseCustomAIs ? 104 : 116;
+
+    public override void Load()
+    {
+        base.Load();
+
+        MonoModHooks.Modify(
+            typeof(AvatarRift).GetMethod(nameof(AvatarRift.DoBehavior_FlailingFrenzy), BindingFlags.Public | BindingFlags.Instance)!,
+            MoreFrequentFlailingFrenzyPortals
+        );
+    }
+
+    private static void MoreFrequentFlailingFrenzyPortals(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        const int portal_summon_delay = 76;
+        const int portal_summon_rate = 8;
+        const int portal_lifetime = 116;
+
+        c.GotoNext(MoveType.After, x => x.MatchLdcI4(portal_summon_delay));
+        c.EmitPop();
+        c.EmitDelegate(() => PortalSummonDelay);
+
+        c.GotoNext(MoveType.After, x => x.MatchLdcI4(portal_summon_rate));
+        c.EmitPop();
+        c.EmitDelegate(() => PortalSummonRate);
+
+        c.GotoNext(MoveType.After, x => x.MatchLdcI4(portal_lifetime));
+        c.EmitPop();
+        c.EmitDelegate(() => PortalLifetime);
+    }
+}
