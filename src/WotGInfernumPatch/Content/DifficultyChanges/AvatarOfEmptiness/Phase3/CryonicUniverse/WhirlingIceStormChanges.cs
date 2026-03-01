7935499e51a97b11d432256366ee8fc16260434f
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-01 03:45:50 -0600

    feat: more aggressive whirling ice storm attack again to reduce cheese potential

--- src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/CryonicUniverse/WhirlingIceStormChanges.cs
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/CryonicUniverse/WhirlingIceStormChanges.cs
@@ -14,9 +14,9 @@ internal sealed class WhirlingIceStormChanges : ModSystem
 {
     public static float InitialArcticBlastMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 1.5f : 1f;
 
-    public static float BlastReleaseRateMultiplier => 1f; /*InfernumMode.InfernumMode.CanUseCustomAIs ? 0.33f : 1f;*/
+    public static float BlastReleaseRateMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.67f : 1f;
 
-    public static float FrostColumnReleaseRateMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.75f : 1f;
+    public static float FrostColumnReleaseRateMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.55f : 1f;
 
     public override void Load()
     {
