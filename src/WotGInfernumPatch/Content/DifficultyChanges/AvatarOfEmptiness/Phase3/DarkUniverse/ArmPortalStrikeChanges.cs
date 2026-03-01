6db111fc69d7712a0b90f4b70179b05ef0616f75
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-01 03:24:06 -0600

    feat: make phase 3 dark universe arm portals spawn more frequently and attack faster

---
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/DarkUniverse/ArmPortalStrikeChanges.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using MonoMod.Cil;
+using Terraria.ModLoader;
+using Avatar = NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm.AvatarOfEmptiness;
+
+namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase3;
+
+internal sealed class ArmPortalStrikeChanges : ModSystem
+{
+    public static int OverriddenPortalSummonRate => 1;
+
+    public static float PortalLifetimeMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.75f : 1f;
+
+    public override void Load()
+    {
+        base.Load();
+
+        MonoModHooks.Modify(
+            typeof(Avatar).GetMethod(nameof(Avatar.DoBehavior_ArmPortalStrikes), BindingFlags.Public | BindingFlags.Instance)!,
+            ArmPortalStrikes_SpawnMorePortalsMoreAggressively
+        );
+    }
+
+    private static void ArmPortalStrikes_SpawnMorePortalsMoreAggressively(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.ArmPortalStrikes_PortalLifetime)}"));
+        c.EmitDelegate((int portalLifetime) => (int)(portalLifetime * PortalLifetimeMultiplier));
+
+        c.GotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.ArmPortalStrikes_PortalSummonRate)}"));
+        c.EmitDelegate((int portalSummonRate) => InfernumMode.InfernumMode.CanUseCustomAIs ? OverriddenPortalSummonRate : portalSummonRate);
+    }
+}
