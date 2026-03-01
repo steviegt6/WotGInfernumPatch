0ac735d48dbe3305bf80046261e9dd121480992d
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-01 03:03:18 -0600

    feat: more aggressive worldsmash attack

---
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase2/WorldSmashChanges.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using MonoMod.Cil;
+using Terraria.ModLoader;
+using Avatar = NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm.AvatarOfEmptiness;
+
+namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase2;
+
+internal sealed class WorldSmashChanges : ModSystem
+{
+    public static int AdditivePlanetoidSmashCount => InfernumMode.InfernumMode.CanUseCustomAIs ? 2 : 0;
+
+    public static float PlanetoidFlingSpeedMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 1.85f : 1f;
+
+    public override void Load()
+    {
+        base.Load();
+
+        MonoModHooks.Add(
+            typeof(Avatar).GetMethod(nameof(Avatar.LoadState_WorldSmash), BindingFlags.Public | BindingFlags.Instance)!,
+            LoadState_WorldSmash_WithOurValues
+        );
+
+        MonoModHooks.Modify(
+            typeof(Avatar).GetMethod(nameof(Avatar.DoBehavior_WorldSmash), BindingFlags.Public | BindingFlags.Instance)!,
+            WorldSmash_FasterAndMorePlentifulPlanetoids
+        );
+    }
+
+    private static void LoadState_WorldSmash_WithOurValues(Avatar self)
+    {
+        self.StateMachine.RegisterTransition(
+            Avatar.AvatarAIType.WorldSmash,
+            null,
+            false,
+            () => self.WorldSmash_PlanetoidSummonCounter >= (Avatar.WorldSmash_PlanetoidSmashCount + AdditivePlanetoidSmashCount) && self.AITimer >= Avatar.WorldSmash_AttackTransitionDelay,
+            () =>
+            {
+                self.WorldSmash_PlanetoidFlyOffDirection = 0f;
+                self.WorldSmash_PlanetoidSummonCounter = 0f;
+            }
+        );
+
+        // Load the AI state behavior.
+        self.StateMachine.RegisterStateBehavior(Avatar.AvatarAIType.WorldSmash, self.DoBehavior_WorldSmash);
+
+        self.StatesToNotStartTeleportDuring.Add(Avatar.AvatarAIType.WorldSmash);
+    }
+
+    private static void WorldSmash_FasterAndMorePlentifulPlanetoids(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        while (c.TryGotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.WorldSmash_PlanetoidSmashCount)}")))
+        {
+            c.EmitDelegate((int smashCount) => smashCount + AdditivePlanetoidSmashCount);
+        }
+
+        c.Index = 0;
+
+        while (c.TryGotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.WorldSmash_PlanetoidFlingSpeed)}")))
+        {
+            c.EmitDelegate((float flingSpeed) => flingSpeed * PlanetoidFlingSpeedMultiplier);
+        }
+    }
+}
