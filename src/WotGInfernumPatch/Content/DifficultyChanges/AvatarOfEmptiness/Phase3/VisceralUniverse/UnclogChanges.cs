13b1f17e5c2d8a04c29397d1964d70ad1eff058d
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-01 03:30:37 -0600

    feat: increase blood blob velocity

--- src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/VisceralUniverse/UnclogChanges.cs
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase3/VisceralUniverse/UnclogChanges.cs
@@ -1,5 +1,9 @@
 ﻿using System.Reflection;
+using Luminance.Common.Utilities;
+using Microsoft.Xna.Framework;
 using MonoMod.Cil;
+using NoxusBoss.Content.NPCs.Bosses.Avatar.Projectiles;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Avatar = NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm.AvatarOfEmptiness;
 
@@ -8,6 +12,8 @@ namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase3;
 internal sealed class UnclogChanges : ModSystem
 {
     public static float BlobSpawnRateMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 1.75f : 1f;
+
+    public static float BlobSpawnVelocityMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 1.45f : 1f;
 
     public override void Load()
     {
@@ -23,7 +29,50 @@ internal sealed class UnclogChanges : ModSystem
     {
         var c = new ILCursor(il);
 
+        // Make them spawn more frequently.
         c.GotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.Unclog_BlobSpawnRate)}"));
         c.EmitDelegate((int blobSpawnRate) => (int)(blobSpawnRate * BlobSpawnRateMultiplier));
+
+        // Increase their initial velocity.
+        c.Index = 0;
+
+        while (c.TryGotoNext(MoveType.Before, x => x.MatchCall(typeof(Utilities), nameof(Utilities.NewProjectileBetter))))
+        {
+            c.Remove();
+
+            c.EmitDelegate(
+                (
+                    IEntitySource source,
+                    Vector2 center,
+                    Vector2 velocity,
+                    int type,
+                    int damage,
+                    float knockback,
+                    int owner,
+                    float ai0,
+                    float ai1,
+                    float ai2
+                ) =>
+                {
+                    if (type == ModContent.ProjectileType<BloodBlob>())
+                    {
+                        velocity *= BlobSpawnVelocityMultiplier;
+                    }
+
+                    Utilities.NewProjectileBetter(
+                        source,
+                        center,
+                        velocity,
+                        type,
+                        damage,
+                        knockback,
+                        owner,
+                        ai0,
+                        ai1,
+                        ai2
+                    );
+                }
+            );
+        }
     }
 }
