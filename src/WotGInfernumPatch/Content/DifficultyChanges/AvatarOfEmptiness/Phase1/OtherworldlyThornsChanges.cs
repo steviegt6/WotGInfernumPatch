083798ed42eb91f87a025f39c07fc74b249d7f1d
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-01 02:24:22 -0600

    feat: speed up otherworldly thorn telegraph

--- src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase1/OtherworldlyThornsChanges.cs
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase1/OtherworldlyThornsChanges.cs
@@ -1,7 +1,9 @@
 ﻿using System.Reflection;
 using MonoMod.Cil;
+using NoxusBoss.Content.NPCs.Bosses.Avatar.Projectiles;
 using Terraria.ModLoader;
 using AvatarRift = NoxusBoss.Content.NPCs.Bosses.Avatar.FirstPhaseForm.AvatarRift;
+using Avatar = NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm.AvatarOfEmptiness;
 
 namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase1;
 
@@ -11,17 +13,24 @@ internal sealed class OtherworldlyThornsChanges : ModSystem
 
     public static int PortalSummonTime => InfernumMode.InfernumMode.CanUseCustomAIs ? 45 : 60;
 
+    public static float ThornReachTelegraphTimeReductionMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 0.75f : 1f;
+
     public override void Load()
     {
         base.Load();
 
         MonoModHooks.Modify(
             typeof(AvatarRift).GetMethod(nameof(AvatarRift.DoBehavior_ReleaseOtherworldlyThorns), BindingFlags.Public | BindingFlags.Instance)!,
-            FasterOtherworldlyThorns
+            FasterOtherworldlyThornsInitiation
+        );
+
+        MonoModHooks.Modify(
+            typeof(OtherworldlyThorn).GetMethod(nameof(OtherworldlyThorn.AI), BindingFlags.Public | BindingFlags.Instance)!,
+            FasterOtherworldlyThornsTelegraph
         );
     }
 
-    private static void FasterOtherworldlyThorns(ILContext il)
+    private static void FasterOtherworldlyThornsInitiation(ILContext il)
     {
         var c = new ILCursor(il);
 
@@ -34,5 +43,14 @@ internal sealed class OtherworldlyThornsChanges : ModSystem
         c.GotoNext(MoveType.After, x => x.MatchLdcI4(60));
         c.EmitPop();
         c.EmitDelegate(() => PortalSummonTime);
+    }
+
+    private static void FasterOtherworldlyThornsTelegraph(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(x => x.MatchLdstr("ReleaseOtherworldlyThorns_ThornReachTelegraphTime"));
+        c.GotoNext(MoveType.After, x => x.MatchCall<Avatar>(nameof(Avatar.GetAIInt)));
+        c.EmitDelegate((int thornReachTelegraphTime) => (int)(thornReachTelegraphTime * ThornReachTelegraphTimeReductionMultiplier));
     }
 }
