6e314c2b209ffbe867427686421cf63893acf437
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-01 02:54:04 -0600

    feat: spawn some more rubble from the gravity slam

---
+++ src/WotGInfernumPatch/Content/DifficultyChanges/AvatarOfEmptiness/Phase2/RubbleGravitySlamChanges.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using MonoMod.Cil;
+using Terraria.ModLoader;
+using Avatar = NoxusBoss.Content.NPCs.Bosses.Avatar.SecondPhaseForm.AvatarOfEmptiness;
+
+namespace WotGInfernumPatch.Content.DifficultyChanges.AvatarOfEmptiness.Phase2;
+
+internal sealed class RubbleGravitySlamChanges : ModSystem
+{
+    public static float RubbleCountMultiplier => InfernumMode.InfernumMode.CanUseCustomAIs ? 1.33f : 1f;
+
+    public override void Load()
+    {
+        base.Load();
+
+        MonoModHooks.Modify(
+            typeof(Avatar).GetMethod(nameof(Avatar.DoBehavior_RubbleGravitySlam_MoveRubble), BindingFlags.Public | BindingFlags.Instance)!,
+            MoveRubble_SpawnMoreRubble
+        );
+    }
+
+    private static void MoveRubble_SpawnMoreRubble(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(MoveType.After, x => x.MatchCall<Avatar>($"get_{nameof(Avatar.RubbleGravitySlam_RubbleCount)}"));
+        c.EmitDelegate((int rubbleCount) => (int)(rubbleCount * RubbleCountMultiplier));
+    }
+}
